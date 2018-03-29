c679074d71a06b3846c51b99c594a2c5d9553fef
zhanghao <zhanghao@zcool.com.cn>
2018-03-29 17:59:24 +0800

    Remove unused debug code

--- src/js/components/user.jsx
+++ src/js/components/user.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 // middleware
+import axios from 'axios';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
@@ -16,12 +17,9 @@ export default class Users extends React.Component {
   }
 
   componentDidMount() {
-    this.userStore.dispatch((dispatch) => {
-      dispatch({ type: 'FETCH_USER_PENDING' });
-      fetch('https://randomuser.me/api/?results=10')
-        .then(resp => resp.json())
-        .then(result => dispatch({ type: 'FETCH_USER_FULFILLED', payload: result.results }))
-        .catch(error => dispatch({ type: 'FETCH_USER_REJECTED', payload: error }));
+    this.userStore.dispatch({
+      type: 'FETCH_USER',
+      payload: axios.get('https://randomusersadfa.me/api/?results=10').then(resp => resp.data.results),
     });
   }
 
@@ -46,7 +44,7 @@ export default class Users extends React.Component {
         };
       }
       case 'FETCH_USER_REJECTED': {
-        return { ...state, fetching: true, error: action.payload };
+        return { ...state, fetching: false, error: action.payload };
       }
       default:
         return state;
@@ -54,7 +52,6 @@ export default class Users extends React.Component {
   }
 
   handleChange = () => {
-    console.log('----------Handle change ', this.userStore.getState());
     this.setState(this.userStore.getState());
   }
 
@@ -62,10 +59,11 @@ export default class Users extends React.Component {
     const {
       users, fetching, error, fetched,
     } = this.state;
+
     if (fetching) {
       return <div>loading...</div>;
     } else if (error) {
-      return <div>{error.message}</div>;
+      return <div>{error.stack}</div>;
     } else if (fetched) {
       return (
         <div className="user-page">
